83472ab2ea72c3cbe51b73ce525293265f4a5e8a
aereal <aereal@aereal.org>
2021-06-24 22:36:43 +0900

    feat: add get reservations scenario

---
+++ pkgs/ginou/src/scenarios/get-reservations.ts
@@ -0,0 +1,88 @@
+import { eachSlice } from "@aereal/enumerable"
+import { parse } from "date-fns"
+import { utcToZonedTime } from "date-fns-tz"
+import { ElementHandle } from "puppeteer-core"
+import { isPresent } from "../present"
+import { LoggedIn } from "./login"
+import { State } from "./types"
+
+const getReservationsType: unique symbol = Symbol()
+
+interface Reservation {
+  readonly date: Date
+  readonly startTime: Date
+  readonly finishTime: Date
+  readonly subject: string
+}
+
+interface GetReservationsPayload {
+  readonly reservations: Reservation[]
+}
+
+export type GotReservations = State<
+  typeof getReservationsType,
+  GetReservationsPayload
+>
+
+type Prev = LoggedIn
+
+const parseDate = (s: string): Date => parse(s, "yyyy/MM/dd", new Date())
+
+const parseTime = (s: string, base: Date): Date => parse(s, "HH:mm", base)
+
+export const getReservations = async (prev: Prev): Promise<GotReservations> => {
+  const {
+    payload: { loginFrame },
+  } = prev
+  await loginFrame.click("#btnMenu_YoyakuItiran")
+  await loginFrame.waitForNavigation()
+
+  const pullDownButtons = await loginFrame.$$("#lst_lc .blocks .iconarea") // pull-down buttons
+  await Promise.all(pullDownButtons.map((handle) => handle.click()))
+  await loginFrame.waitForSelector(".slide-down.show") // await animation finish
+
+  const divs = await loginFrame.$$<HTMLDivElement>("#lst_lc > .page > div")
+  const pairs = Array.from(eachSlice(2, divs))
+  const extractReservation = async (
+    pair: ElementHandle<HTMLDivElement>[]
+  ): Promise<Reservation> => {
+    const [dateDiv, scheduleDiv] = pair
+    const rawDate = await dateDiv.evaluate(
+      (el) => el.querySelector(".lbl")?.textContent
+    )
+    if (rawDate === undefined || rawDate === null) {
+      throw new Error("date is undefined")
+    }
+    const date = utcToZonedTime(parseDate(rawDate.slice(0, 10)), "Asia/Tokyo")
+    const [
+      _1,
+      _2,
+      _3,
+      rawStart,
+      _4,
+      rawFinish,
+      subject,
+    ] = await scheduleDiv.evaluate((el) =>
+      Array.from(el.querySelectorAll(".page > div .lbl")).map(
+        (label) => label.textContent?.trim() ?? ""
+      )
+    )
+    const startTime = parseTime(rawStart, date)
+    const finishTime = parseTime(rawFinish, date)
+    return {
+      date,
+      startTime,
+      finishTime,
+      subject,
+    }
+  }
+  const reservations = (
+    await Promise.all(pairs.map(extractReservation))
+  ).filter(isPresent)
+  return {
+    type: getReservationsType,
+    payload: {
+      reservations,
+    },
+  }
+}
