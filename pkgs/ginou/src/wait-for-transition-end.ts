0748d093fd0d6a29604fa12f638dff001a862290
aereal <aereal@aereal.org>
2021-06-27 16:40:10 +0900

    fix: waitForTransitionEnd

---
+++ pkgs/ginou/src/wait-for-transition-end.ts
@@ -0,0 +1,14 @@
+import { ElementHandle } from "puppeteer-core"
+
+export const waitForTransitionEnd = (eh: ElementHandle): Promise<void> =>
+  eh.evaluate(
+    (el: Element) =>
+      new Promise<void>((ok) => {
+        const transitionEnd = "transitionend" as const
+        const onEnd = () => {
+          el.removeEventListener(transitionEnd, onEnd)
+          ok()
+        }
+        el.addEventListener(transitionEnd, onEnd)
+      })
+  )
