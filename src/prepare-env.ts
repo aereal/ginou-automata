c295f21097b128104f16acf611363dc28cc6a931
aereal <aereal@aereal.org>
2021-06-30 01:56:21 +0900

    fix: decode accessed secrets

--- src/prepare-env.ts
+++ src/prepare-env.ts
@@ -1,4 +1,7 @@
-import { SecretManagerServiceClient } from "@google-cloud/secret-manager"
+import {
+  protos,
+  SecretManagerServiceClient,
+} from "@google-cloud/secret-manager"
 
 const [GINOU_LOGIN_ID, GINOU_LOGIN_PASSWORD, GINOU_YOYAKU_URL] = [
   "GINOU_LOGIN_ID",
@@ -42,14 +45,39 @@ const prepareEnvFromSecretManager = async (): Promise<Environment | Error> => {
   const client = new SecretManagerServiceClient()
   const secretNames = [GINOU_LOGIN_ID, GINOU_LOGIN_PASSWORD, GINOU_YOYAKU_URL]
   const reqs = secretNames.map(async (name) => {
-    const [secret] = await client.accessSecretVersion({ name })
+    let secret:
+      | protos.google.cloud.secretmanager.v1.IAccessSecretVersionResponse
+      | undefined
+    try {
+      const resp = await client.accessSecretVersion({
+        name: `projects/ginou-automata/secrets/${name}/versions/latest`,
+      })
+      secret = resp[0]
+    } catch (e: unknown) {
+      if (e instanceof Error) {
+        return e
+      }
+      return new Error(`unknown error: ${e}`)
+    }
     const data = secret.payload?.data
+    if (data === null || data === undefined) {
+      return new Error(`secret ${name} is empty`)
+    }
     if (typeof data === "string") {
       return data
     }
-    throw new Error(`secret ${name} has invalid payload`)
+    return new TextDecoder().decode(data)
   })
   const [id, password, yoyakuURL] = await Promise.all(reqs)
+  if (id instanceof Error) {
+    return id
+  }
+  if (password instanceof Error) {
+    return password
+  }
+  if (yoyakuURL instanceof Error) {
+    return yoyakuURL
+  }
   return { id, password, yoyakuURL }
 }
 
