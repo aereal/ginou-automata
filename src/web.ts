be310c7011ef0b0877002f454b0585adc06a5f5f
aereal <aereal@aereal.org>
2021-06-29 23:49:46 +0900

    feat: /run and /

--- src/web.ts
+++ src/web.ts
@@ -1,13 +1,18 @@
-import { createServer } from "http"
+import { createServer, IncomingMessage, ServerResponse } from "http"
 import { performance } from "perf_hooks"
 import { main } from "./main"
+import { MissingRequiredParameterError, prepareEnv } from "./prepare-env"
 
 const { PORT } = process.env
 const port = parseInt(PORT ?? "")
 if (isNaN(port)) {
   throw new Error(`Invalid PORT: ${PORT}`)
 }
-const server = createServer(async (req, res) => {
+
+const handleRun = async (
+  _: IncomingMessage,
+  res: ServerResponse
+): Promise<void> => {
   const from = performance.now()
   await main()
   const done = performance.now()
@@ -17,6 +22,32 @@ const server = createServer(async (req, res) => {
     "content-length": Buffer.byteLength(body, "utf-8"),
   })
   res.end(body)
+}
+
+const handleDefault = async (
+  _: IncomingMessage,
+  res: ServerResponse
+): Promise<void> => {
+  const envOrError = prepareEnv()
+  const body = JSON.stringify({
+    ok: MissingRequiredParameterError.is(envOrError),
+  })
+  res.writeHead(200, {
+    "content-type": "application/json",
+    "content-length": Buffer.byteLength(body, "utf-8"),
+  })
+  res.end(body)
+}
+
+const server = createServer(async (req, res) => {
+  switch (`${req.method} ${req.url}`) {
+    case "GET /run":
+      await handleRun(req, res)
+      break
+    default:
+      await handleDefault(req, res)
+      break
+  }
 })
 console.log(`accepting requests on :${port}`)
 server.listen(port)
