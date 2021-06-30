cf6f65991e355b72f41676c262f16e7c40ffda03
aereal <aereal@aereal.org>
2021-07-01 00:03:11 +0900

    feat(terraform): add IAM policies

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,3 +15,17 @@ resource "google_service_account" "cloud_run_runner" {
   account_id   = "cloud-run-runner"
   display_name = "cloud-run-runner"
 }
+
+resource "google_project_iam_member" "cloud_run_runner_invoker" {
+  role   = "roles/run.invoker"
+  member = "serviceAccount:${google_service_account.cloud_run_runner.email}"
+}
+
+resource "google_project_iam_member" "cloud_run_runner_secret_accessor" {
+  role   = "roles/secretmanager.secretAccessor"
+  member = "serviceAccount:${google_service_account.cloud_run_runner.email}"
+  condition {
+    title      = "allow_access_to_ginou-related_secrets"
+    expression = "resource.name.startsWith(\"GINOU_\")"
+  }
+}
