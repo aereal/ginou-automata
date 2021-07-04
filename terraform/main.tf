6b667fbe14cff0b029f0b1f18d448fc9ae293133
aereal <aereal@aereal.org>
2021-07-05 00:00:37 +0900

    fix(terraform): add iam.serviceAccountUser role to Actions runner role

--- terraform/main.tf
+++ terraform/main.tf
@@ -65,6 +65,11 @@ resource "google_project_iam_member" "github_actions_runner_run_admin" {
   member = "serviceAccount:${google_service_account.github_actions_runner.email}"
 }
 
+resource "google_project_iam_member" "github_actions_runner_sa_actor" {
+  role   = "roles/iam.serviceAccountUser"
+  member = "serviceAccount:${google_service_account.github_actions_runner.email}"
+}
+
 resource "google_service_account_key" "github_actions_runner" {
   service_account_id = google_service_account.github_actions_runner.id
 }
